f98354c5b978dfe16e01b5340e384be9bdc6867e
TThom66 <tradd.dylan.thomas@gmail.com>
2026-04-28 09:21:08 -0400

    Added a users table

--- models.py
+++ models.py
@@ -1,7 +1,17 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from database import Base
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False, index=True)
+    hashed_password = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    documents = relationship("Document", back_populates="owner")
+    
 class Document(Base):
     __tablename__ = "documents"
 
@@ -9,3 +19,5 @@ class Document(Base):
     title = Column(String, nullable=False)
     content = Column(Text, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    owner = relationship("User", back_populates="documents")
