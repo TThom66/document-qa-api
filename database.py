bce884526f305b450c6650139be843a444d990d6
TThom66 <tradd.dylan.thomas@gmail.com>
2026-05-08 09:25:08 -0400

    Replaced SQLite with PostgreSQL and added Alembic as a migration tool

--- database.py
+++ database.py
@@ -1,14 +1,27 @@
+# Import Statements
+import os
+import logging
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./documents.db"
+# Set up logger
+load_dotenv()
+logger = logging.getLogger(__name__)
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, 
-    connect_args={"check_same_thread": False}
-)
+#Declare Database URL
+DATABASE_URL = os.getenv("DATABASE_URL")
 
+# Check if URL is valid
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable not set")
+
+# Railway provides postgres:// but SQLAlchemy needs postgresql://
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
